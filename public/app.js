92aec64d0d08e00983f927f626de86a288f95742
James Scott <scottj1426@gmail.com>
2017-10-03 16:46:47 -0500

    views done

--- public/app.js
+++ public/app.js
@@ -1,1 +1,36 @@
-angular.module('eStore', ['ui.router']);
+angular.module('eStore', ['ui.router'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+    .state("Login", {
+        url: "/login",
+        templateUrl: "/public/component/login/login.html"
+      })
+      .state("Home", {
+        url: "/home",
+        templateUrl: "/public/component/home/home.html"
+      })
+
+      .state("Inventory", {
+        url: "/Inventory",
+        templateUrl: "/public/component/inventory/products.html"
+      })
+      .state("ShoppingCart", {
+        url: "/Cart",
+        templateUrl: "/public/component/shoppingCart/shoppingCart.html"
+      })
+      .state("Checkout", {
+        url: "/Checkout",
+        templateUrl: "/public/component/checkout/checkout.html"
+      })
+      .state("Profile", {
+        url: "/Profile",
+        templateUrl: "/public/component/profile/profile.html"
+      });
+      
+
+
+  });

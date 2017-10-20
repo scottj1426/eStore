d1bfb948595380e2dfb2bcf163a192dbab49b221
James Scott <scottj1426@gmail.com>
2017-10-20 18:25:04 -0500

    updats

--- db/cartToOrders.sql
+++ db/cartToOrders.sql
@@ -1,2 +1,3 @@
-INSERT INTO orders (name, price, quantity, image, authid,date) VALUES ($1, $2, $3, $4, $5,$6);
+INSERT INTO orders (name, price, quantity, image, authid, date) VALUES ($1, $2, $3, $4, $5,$6);
+
 

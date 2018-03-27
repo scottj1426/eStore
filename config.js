c750f56f3c5f59c0931c41331537afbdacb3fdb6
James Scott <scottj1426@gmail.com>
2018-03-27 17:13:33 -0500

    updating

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    session: {
+            secret: 'youcantfigureitout'
+    },
+
+    db: {
+        dbUSer: 'JamesScott',
+        database: 'JamesScott'
+    },
+
+    auth0: {
+        domain: 'jamescott.auth0.com',
+        clientID: 'ElSPdx2CL1sfcAVc0gTe5zeRrChaIGau',
+        clientSecret: 'APE0r1t5zgp4d6ejoHZ0u-oR-qZinRhuweyASmjYg3pHXCHLz9Cipj_IQOiOGUor'
+    },
+    keys: {
+        stripePublicKey: 'pk_test_AcVYHno8m2zXni1DEdMDF2hL',
+        stripeSecretKey: 'sk_test_cW2ePkIYoNH0QHyItv6PcYI9'
+
+    }
+}
